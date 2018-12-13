dbd3b4455748506ed27792716a1829d777132429
halahhosny <45852598+halahhosny@users.noreply.github.com>
2018-12-13 23:27:26 +0200

    Book , Category Models

---
+++ BookStore/BookStore_Core_Project/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_Core_Project.Models
+{
+    public class Book
+    {
+
+        [Key]
+        public int BookID { get; set; }
+
+
+        public string Name { get; set; }
+
+
+        public decimal Price { get; set; }
+        
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public Category  Category   {get; set;}
+        
+    }
+}
---
+++ BookStore/BookStore_Core_Project/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_Core_Project.Models
+{
+    public class Category
+    {
+
+        
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+
+        
+        
+        
+    }
+}

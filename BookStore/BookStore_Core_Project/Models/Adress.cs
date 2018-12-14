eb37ac2245c15a8edad379bcb30f639b95591d80
halahhosny <45852598+halahhosny@users.noreply.github.com>
2018-12-14 15:37:31 +0200

    Add 2 models extra

---
+++ BookStore/BookStore_Core_Project/Models/Adress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_Core_Project.Models
+{
+    public class Adress
+    {
+        public int AdressID { get; set; }
+        public string Location { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ BookStore/BookStore_Core_Project/Models/users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_Core_Project.Models
+{
+    public class users
+    {
+        public int userID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+
+    }
+}
